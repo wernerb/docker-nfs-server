c8f20b28fbf12d69bbc03b5046bacaf7b2d31fbf
Werner Buck <wernerbuck@gmail.com>
2015-09-12 18:45:07 +0200

    Changed default mount optinos

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -6,7 +6,7 @@ mounts="${@}"
 
 for mnt in "${mounts[@]}"; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
-  echo "$src *(rw,sync,no_subtree_check,fsid=0,no_root_squash)" >> /etc/exports
+  echo "$src *(rw,sync,all_squash,fsid=0,no_subtree_check,insecure,anonuid=1000,anongid=100)" >> /etc/exports
 done
 
 exec runsvdir /etc/sv
